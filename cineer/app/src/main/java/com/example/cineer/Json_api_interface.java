050cc7f4967c08db47e054bfca8badcde5ee07bd
f3Llx <45992148+f3Llx@users.noreply.github.com>
2020-05-17 23:44:40 +0200

    FirstCommit
    :+1:

---
+++ cineer/app/src/main/java/com/example/cineer/Json_api_interface.java
@@ -0,0 +1,12 @@
+package com.example.cineer;
+
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Json_api_interface {
+
+    //Todo Aqui van las llamadas de la api
+
+}
