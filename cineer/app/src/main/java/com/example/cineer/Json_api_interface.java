19e068d0d95f75597388369afef983f1b6b22d8e
f3Llx <45992148+f3Llx@users.noreply.github.com>
2020-05-20 23:57:13 +0200

    Solucionado movies info problemas!
    problemas de version solucionados

--- cineer/app/src/main/java/com/example/cineer/Json_api_interface.java
+++ cineer/app/src/main/java/com/example/cineer/Json_api_interface.java
@@ -1,7 +1,10 @@
 package com.example.cineer;
 import com.example.cineer.Movies_Popular.Movies_popular;
+import com.example.cineer.Movies_info.Movies_Popular_Details;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Json_api_interface {
@@ -13,8 +16,9 @@ public interface Json_api_interface {
     Call<Movies_popular> getPopularMovies(@Query("api_key") String key,@Query("page") int page);
 
     //peticion para detalles de pelicula
-    @GET("movie")
-    Call<Movies_popular> getmovieDetails(@Query("api_key") String key);
+    @GET("movie/{movie_id}")
+    Call<Movies_Popular_Details> getmovieDetails(@Path("movie_id") int id, @Query("api_key") String key);
+
 
 
 }
